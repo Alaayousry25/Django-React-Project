7c26c23b0cab5c5bbb2f3f0ffc6e73740552f511
Alaa Yousry <Alaayouary303@gmail.com>
2025-09-10 09:32:42 +0300

    fifth commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,19 +1,21 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
-const api= axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+const apiUrl = "/choreo-apis/django-react-project/backend/v1"
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl
 })
 
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem(ACCESS_TOKEN);
         if (token) {
-            config.headers.Authorization= `Bearer ${token}`
+            config.headers.Authorization = `Bearer ${token}`
         }
         return config
-    }, 
-    (error) =>{
+    },
+    (error) => {
         return Promise.reject(error)
     }
 )
